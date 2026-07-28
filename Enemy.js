02796a92e51dc44e47e0dfe52568c647dd3b83fd
axell152 <axell.nicol@outlook.fr>
2026-07-28 08:36:21 +0200

    Delete Enemy.js

--- Enemy.js
+++
@@ -1,5 +0,0 @@
-import Phaser from 'phaser';
-
-// Single shared emitter so GameScene and UIScene can talk without
-// reaching into each other directly.
-export const EventBus = new Phaser.Events.EventEmitter();
